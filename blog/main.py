dd4f84cde3da2bcdea4623232ab93aafec0bf419
Brenno <brenno1804@gmail.com>
2021-08-11 17:10:54 -0300

    Playing with Users table

--- blog/main.py
+++ blog/main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from .hashing import Hash
+
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -80,13 +82,26 @@ def get_blog(id: int, response: Response, db: Session = Depends(get_db)):
     return blog
 
 
-@app.post('/user', status_code=status.HTTP_201_CREATED)
+@app.post('/user',
+          status_code=status.HTTP_201_CREATED,
+          response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
     new_user = models.User(name=request.name,
                            email=request.email,
-                           password=request.password)
+                           password=Hash.bcrypt(request.password))
 
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.get('/user/{id}',
+         status_code=status.HTTP_200_OK,
+         response_model=schemas.ShowUser)
+def get_user(id: int, response: Response, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'User with id {id} is not available')
+    return user
